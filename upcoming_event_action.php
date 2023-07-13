4e50f133266af5efa63b952e4c3f823d459c7953
Mohan Subudhi <111859729+Spupun@users.noreply.github.com>
2023-07-14 00:12:15 +0530

    Create upcoming_event_action.php

---
+++ upcoming_event_action.php
@@ -0,0 +1,22 @@
+<?php
+
+include "config.php";
+
+if (isset($_POST['submit'])) {
+   $event_name = $_POST['event_name'];
+   $event_desc = $_POST['event_desc'];
+   $image = $_FILES['image']['name'];
+   $date=$_POST['date'];
+
+   $sql = "INSERT INTO `upcoming_events`(event_name, event_desc,image,date) VALUES ('$event_name', '$event_desc', '$image', '$date')";
+
+   $result = mysqli_query($conn, $sql);
+
+   if ($result) {
+      //echo "New record added successfully.";
+      header('location:view_events.php');
+      move_uploaded_file($_FILES['image']['tmp_name'], "image/$image");
+   } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+   }
+}
