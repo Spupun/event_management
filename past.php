6fc946e9aedb9d67cb5dcc1ab771dfc9d401fd26
Mohan Subudhi <111859729+Spupun@users.noreply.github.com>
2023-07-20 15:14:21 +0530

    Update and rename past_event.php to past.php

--- past.php
+++ past.php
@@ -3,23 +3,12 @@ include "config.php";
 
 if (isset($_GET)) {
     $id = $_GET['pastid'];
-    // $upcoming_events = "upcoming_events";
-    // $past_events = "past_events";
+
     $selectQuery = "SELECT * FROM upcoming_events where id ='$id'";
     $result = mysqli_query($conn, $selectQuery);
-    // echo '<pre>';print_r($result);die;
 
     if ($result && mysqli_num_rows($result) > 0) {
-        // while ($row = mysqli_fetch_assoc($result)) {
-            // $event_name = $row['event_name'];
-            // $event_desc = $row['event_desc'];
-            // $image = $row['image'];
-            // $date = $row['date'];
-            // $move_past = $row['move_past'];
-
-
-            // $insertQuery = "INSERT INTO $past_events (event_name, event_desc,image,date) VALUES ('$event_name', '$event_desc','$image','$date')";
-
+        
             $updateQuery = "UPDATE `upcoming_events` SET `move_past` = 1 where id ='$id'";
 
             $result = mysqli_query($conn, $updateQuery);
@@ -41,7 +30,5 @@ if (isset($_GET)) {
                 }
            };
           
-    } else {
-        echo "Error selecting records: " . mysqli_error($conn);
-    }
+    } 
 mysqli_close($conn);
